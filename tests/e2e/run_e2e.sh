b3c252ee78c8fb262224f2436fd9e4abcdf8a1ed
FL42 <46161216+fl42@users.noreply.github.com>
2025-07-12 00:25:28 +0200

    fix: tests

--- tests/e2e/run_e2e.sh
+++ tests/e2e/run_e2e.sh
@@ -18,10 +18,10 @@ docker logs $CONTAINER_NAME
 
 METRICS=$(curl -s http://localhost:8080/metrics | grep go_monitoring_up)
 
-grep "go_monitoring_up{id=\"github.com:22\",name=\"\",probe=\"raw_tcp\"} 1" <<< "$METRICS"
-grep "go_monitoring_up{id=\"127.0.0.1\",name=\"\",probe=\"ping\"} 1" <<< "$METRICS"
-grep "go_monitoring_up{id=\"1.2.3.4\",name=\"\",probe=\"ping\"} 0" <<< "$METRICS"
-grep "go_monitoring_up{id=\"https://google.com\",name=\"test_200\",probe=\"http\"} 1" <<< "$METRICS"
+grep "go_monitoring_up{filename=\"/config/config.yaml\",id=\"github.com:22\",name=\"\",oncall_offer=\"day\",probe=\"raw_tcp\"} 1" <<< "$METRICS"
+grep "go_monitoring_up{filename=\"/config/config.yaml\",id=\"127.0.0.1\",name=\"\",oncall_offer=\"day\",probe=\"ping\"} 1" <<< "$METRICS"
+grep "go_monitoring_up{filename=\"/config/config.yaml\",id=\"1.2.3.4\",name=\"\",oncall_offer=\"day\",probe=\"ping\"} 0" <<< "$METRICS"
+grep "go_monitoring_up{filename=\"/config/config.yaml\",id=\"https://google.com\",name=\"test_200\",oncall_offer=\"day\",probe=\"http\"} 1" <<< "$METRICS"
 # httpstat.us is down
 # grep "go_monitoring_up{id=\"https://httpstat.us/403\",name=\"\",probe=\"http\"} 0" <<< "$METRICS"
 # grep "go_monitoring_up{id=\"https://httpstat.us/404\",name=\"\",probe=\"http\"} 1" <<< "$METRICS"
