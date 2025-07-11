e285fc042f6b69d39aa1931c2efdce80743752c9
FL42 <46161216+fl42@users.noreply.github.com>
2025-07-11 23:56:42 +0200

    fix: disable broken e2e

--- tests/e2e/run_e2e.sh
+++ tests/e2e/run_e2e.sh
@@ -22,8 +22,9 @@ grep "go_monitoring_up{id=\"github.com:22\",name=\"\",probe=\"raw_tcp\"} 1" <<< 
 grep "go_monitoring_up{id=\"127.0.0.1\",name=\"\",probe=\"ping\"} 1" <<< "$METRICS"
 grep "go_monitoring_up{id=\"1.2.3.4\",name=\"\",probe=\"ping\"} 0" <<< "$METRICS"
 grep "go_monitoring_up{id=\"https://google.com\",name=\"test_200\",probe=\"http\"} 1" <<< "$METRICS"
-grep "go_monitoring_up{id=\"https://httpstat.us/403\",name=\"\",probe=\"http\"} 0" <<< "$METRICS"
-grep "go_monitoring_up{id=\"https://httpstat.us/404\",name=\"\",probe=\"http\"} 1" <<< "$METRICS"
+# httpstat.us is down
+# grep "go_monitoring_up{id=\"https://httpstat.us/403\",name=\"\",probe=\"http\"} 0" <<< "$METRICS"
+# grep "go_monitoring_up{id=\"https://httpstat.us/404\",name=\"\",probe=\"http\"} 1" <<< "$METRICS"
 
 echo "Tests passed!"
 
